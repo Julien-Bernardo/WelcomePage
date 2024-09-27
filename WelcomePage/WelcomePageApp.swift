ae2621007957d945eca2453f85c0f3aaaa646571
Julien-Bernardo <jfb2hk@gmail.com>
2024-09-27 11:08:29 +0100

    Initial Commit

---
+++ WelcomePage/WelcomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomePageApp.swift
+//  WelcomePage
+//
+//  Created by Julien Bernardo on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct WelcomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
